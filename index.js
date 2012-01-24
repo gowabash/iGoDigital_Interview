14a9361d1d6bd33fb79bd8369d8829e6361fa9ab
James Dunn <jamesldunnjr@gmail.com>
2012-01-23 20:03:18 -0500

    set default route to fib_sum

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ var server = require("./server");
 var router = require("./router");
 
 var handler = require("./requestHandler");
-handler["/"] = handler.example;
-//handler["/example"] = handler.example;
+handler["/"] = handler.fib_sum;
 handler["/fib_sum"] = handler.fib_sum;
 
 server.start(router.route, handler);
