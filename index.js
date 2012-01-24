160a993874f81dfe8ce8676613550e8ee568f496
James Dunn <jamesldunnjr@gmail.com>
2012-01-23 19:52:13 -0500

    implementing /fib_sum?limit=x

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var router = require("./router");
 
 var handler = require("./requestHandler");
 handler["/"] = handler.example;
-handler["/example"] = handler.example;
+//handler["/example"] = handler.example;
+handler["/fib_sum"] = handler.fib_sum;
 
 server.start(router.route, handler);
